ed482e0fc07fb2660377aa89af715aece2b251bc
rslavin <rocky.slavin@gmail.com>
2021-12-01 09:15:03 -0600

    shaved off some parentheses

--- day01/day1.py
+++ day01/day1.py
@@ -1,4 +1,4 @@
 i = list(map(int, open('day1.in')))
-print(sum((b > a for (a, b) in zip(i, i[1:]))))
+print(sum(b > a for a, b in zip(i, i[1:])))
 j = list(sum(k) for k in zip(i, i[1:], i[2:]))
-print(sum((b > a for (a, b) in zip(j, j[1:]))))
+print(sum(b > a for a, b in zip(j, j[1:])))
